930d73a6516027e78e5be6d750655513e9bdb219
Juanda9992 <juanda999games@gmail.com>
2022-07-07 16:00:50 -0500

    BUILD 4
    Added Button Obstacle, the player has to jump over it to toggle the door

---
+++ Assets/Scripts/Enviroment/Obstacle/Button_Obstacle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Obstacle : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject door;
+    private bool inTrigger = false;
+    private void Start()
+    {
+        transform.position = new Vector2(transform.position.x,Random.Range(-5,5));
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inTrigger = true;
+        }    
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            inTrigger = false;
+        }
+    }
+
+    private void toggleDoor()
+    {
+        if(inTrigger)
+        {
+            door.SetActive(false);
+        }    
+    }
+
+    void OnEnable()
+    {
+        Player_Jump.onPlayerJump += toggleDoor;
+    }
+    void OnDisable()
+    {
+        Player_Jump.onPlayerJump -= toggleDoor;
+    }
+}
