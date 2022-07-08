a4580b8b39b0cd6ae9154f5b31f64d8a162c97e8
Juanda9992 <juanda999games@gmail.com>
2022-07-08 14:22:31 -0500

    Build 5
    Added Spawner Script, it creates a platform at a certain time
    
    -Nerfed the obstacles
    -Updated EditorTests

--- Assets/Tests/EditMode/TestCI.cs
+++ Assets/Tests/EditMode/TestCI.cs
@@ -20,4 +20,37 @@ public class TestCI
         Assert.IsTrue((spam.currentDuplicateNumber >= 3) && (spam.currentDuplicateNumber <= spam.maxTimesToDuplicate));
     }
 
+    [Test]
+    [TestCase(1)]
+    [TestCase(0.3f)]
+    [TestCase(2)]
+    [TestCase(0.2f)]
+    public void time_greater_than_zero_not_spawn_object(float number)
+    {
+        var spawner = new GameObject();
+        var _spawner = spawner.AddComponent<Spawner>();
+        _spawner.currentTimeBetweenSpawn = number;
+        _spawner.checkForSpawn();
+        Assert.IsFalse(_spawner.spawned);
+
+    }
+
+    
+    [Test]
+    [TestCase(-1)]
+    [TestCase(-0.3f)]
+    [TestCase(0)]
+    [TestCase(-0.2f)]
+    public void time_smaller_than_zero_spawn_object(float number)
+    {
+        var spawner1 = new GameObject();
+        var _spawner1 = spawner1.AddComponent<Spawner>();
+        _spawner1.timeBetweenSpawn = number;
+        _spawner1.currentTimeBetweenSpawn = number;
+        _spawner1.checkForSpawn();
+        Assert.IsTrue(_spawner1.spawned);
+    }
+
+
+
 }
