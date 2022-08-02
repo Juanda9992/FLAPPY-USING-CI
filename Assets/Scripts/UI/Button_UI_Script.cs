8f91a3dc3e03332729a8fe93272bdb5d107d5bbc
Juanda9992 <juanda999games@gmail.com>
2022-08-02 15:30:10 -0500

    BUILD 18.1
    Added Fade_Canvas singleton, it allows to give a transtion between scenes by fading the screen in and out
    
    Added Button UI Script, it calls the method from the SceneLoader (Unity doesnt allow to reference singletons in UnityEvents)
    
    Changelog
    Scene Loader now is a singleton
    Now all the obstacles are destroyed when the player dies instead of when the level is reloaded.
    When the player dies, the ammo will be 0
    Fixed Critical Bug that reloads the level when the player press Space (it happens beacuese the restart button from the gameover panel gets triggered with space)

--- Assets/Scripts/UI/Button_UI_Script.cs
+++ Assets/Scripts/UI/Button_UI_Script.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Open_Link : MonoBehaviour
+public class Button_UI_Script : MonoBehaviour
 {
+    public void LoadScene(string scene)
+    {
+        Fade_Canvas.fade_canvas_inst.showPanel(scene);
+    }
+
     public void OpenLink(string link)
     {
         Application.OpenURL(link);
---
+++ Assets/Scripts/UI/Fade_Canvas.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class Fade_Canvas : MonoBehaviour
+{
+    public static Fade_Canvas fade_canvas_inst;
+    public CanvasGroup canvas;
+    void Start()
+    {
+        if(fade_canvas_inst == null)
+        {
+            fade_canvas_inst = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void showPanel(string scene)
+    {
+        canvas.DOFade(1,0.2f).OnComplete(()=>Scene_Loader.scene_Loader_inst.LoadScene(scene));
+        canvas.DOFade(0,0.2f).SetDelay(0.2f);
+    }
+
+}
