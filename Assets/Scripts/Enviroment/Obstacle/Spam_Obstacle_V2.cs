8f99ac102216d2883b4a1e26c8d93a742d906884
Juanda9992 <juanda999games@gmail.com>
2022-08-19 16:34:44 -0500

    BETA V1.3
    The game is getting closer to the public version!
    The name of the game is Flappy Dash!!!
    
    ADDITION
    Added Spam Obstacle V2, you need a certain amount of input to disable it
    
    CHANGELOG
    Changed the title of the game in the main menu
    Now when the player loses focus the game will pause
    When the player quits the game it will save
    Added more layers of ground and roof (When the player dies, the camera will zoom in to his position, and the floor was incomplete, this was fixed)

---
+++ Assets/Scripts/Enviroment/Obstacle/Spam_Obstacle_V2.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Spam_Obstacle_V2 : MonoBehaviour
+{
+    [SerializeField]private int maxClicks, minClicks, randomClicks,currentClicks;
+    [SerializeField] private GameObject wallObject;
+    [SerializeField] private TextMeshProUGUI clicksText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomClicks = Random.Range(minClicks,maxClicks);
+        clicksText.text = randomClicks.ToString();
+    }
+
+    void Update()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+    }
+    private void IncrementCLicks()
+    {
+        randomClicks = Mathf.Clamp(randomClicks,1,maxClicks);
+        randomClicks--;
+        clicksText.text = randomClicks.ToString();
+        if(randomClicks <=0)
+        {
+            wallObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        Player_Jump.onPlayerJump += IncrementCLicks;
+    }
+    void OnDisable()
+    {
+        Player_Jump.onPlayerJump -= IncrementCLicks;
+    }
+}
