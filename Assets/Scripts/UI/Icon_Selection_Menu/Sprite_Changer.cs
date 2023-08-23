918bb95b58f8f0fe544f45892241a403548f5643
Juanda999 <juanda999games@gmail.com>
2023-08-23 08:52:10 -0500

    Improve logic for saving / loading the sprites on the icon Selection

--- Assets/Scripts/UI/Icon_Selection_Menu/Sprite_Changer.cs
+++ Assets/Scripts/UI/Icon_Selection_Menu/Sprite_Changer.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 public class Sprite_Changer : MonoBehaviour
 {
     public SpriteRenderer sRenderer;
+    [SerializeField] private DataSprite allSprites;
     [SerializeField] private Sprite defaultSprite;
     private SaveDataHolder dataHolder;
     // Start is called before the first frame update
     void Start()
     {
         dataHolder = GameObject.FindObjectOfType<SaveDataHolder>();
-        sRenderer = GetComponent<SpriteRenderer>();    
+        sRenderer = GetComponent<SpriteRenderer>();  
+
+        LoadSprite();  
     }
 
     public void ChangeSprite(Image spriteImage)
@@ -22,6 +25,11 @@ public class Sprite_Changer : MonoBehaviour
     public void SaveSprite(int spriteIndex)
     {
         dataHolder.data.spriteIndex = spriteIndex;
+    }
+
+    private void LoadSprite()
+    {
+        sRenderer.sprite = allSprites.GetSprite(dataHolder.data.spriteIndex);
     }
 
 }
