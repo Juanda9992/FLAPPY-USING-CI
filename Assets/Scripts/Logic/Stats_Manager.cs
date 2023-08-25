54e4e14ec401b16749f9e820ed2267328ff513ca
Juanda999 <juanda999games@gmail.com>
2023-08-25 08:46:57 -0500

    Update logic on the stats panel
    Now it reads the save model and contains a list of textmeshpro instead of one text for every stat

--- Assets/Scripts/Logic/Stats_Manager.cs
+++ Assets/Scripts/Logic/Stats_Manager.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class Stats_Manager : MonoBehaviour
 {
 
-    public TextMeshProUGUI playTime,jumps,score,highScore,deaths,bombs,hearts;
+    public TextMeshProUGUI[] statTexts;
+
+    public SaveModel saveModel;
 
     void Start()
     {
@@ -15,13 +18,14 @@ public class Stats_Manager : MonoBehaviour
 
     public void UpdateText()
     {
-        playTime.text = "Total Play Time: " + (Stats_Handler.stats_Handler_inst.totalPlayTime / 60).ToString("F2") + "minutes";
-        jumps.text = "Total Jumps: " + Stats_Handler.stats_Handler_inst.totalJumps;
-        score.text = "Total Score: " + Stats_Handler.stats_Handler_inst.totalScore;
-        highScore.text = "Highest Score: " + Stats_Handler.stats_Handler_inst.highestScore;
-        deaths.text = "Total Deaths: " + Stats_Handler.stats_Handler_inst.totalDeaths;
-        bombs.text = "Total Bombs Collected: " + Stats_Handler.stats_Handler_inst.totalBombs;
-        hearts.text = "Total Hearts Collected: " + Stats_Handler.stats_Handler_inst.totalHearts;
+        statTexts[0].text = "Total Play Time: " + TimeSpan.FromSeconds(saveModel.totalTime);
+        statTexts[1].text = "Total Jumps: " + saveModel.totalJumps;
+        statTexts[2].text = "Total Score: " + saveModel.totalScore;
+        statTexts[3].text = "Highest Score: " + saveModel.highestScore;
+        statTexts[4].text = "Total Deaths: " + saveModel.totalDeaths;
+        statTexts[5].text = "Total Bombs Collected: " + saveModel.bombsTaken;
+        statTexts[6].text = "Total Bombs Fired: " + saveModel.bombsShoot;
+        statTexts[7].text = "Total Hearts Collected: " + saveModel.heartsTaken;
     }
 
 }
